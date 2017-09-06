1be34727e6e1b1a2e525186bf49a8897e54faf7e
Robert Pooley <coolosity1998@gmail.com>
2017-09-05 20:37:37 -0400

    Updates AntLabMain with my name

--- M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -57,24 +57,24 @@ import edu.gatech.oad.antlab.pkg2.*;
           + ant22.getMessage() + ant31.getMessage();
 		  //Person1 replace P1 with your name
 		  //and gburdell1 with your gt id
-		  Person1 p1 = new Person1("P1");
-		  toPrint += p1.toString("gburdell1");
+		  Person1 p1 = new Person1("RobertNoScopeM4ST3R42");
+		  toPrint += p1.toString("RobertNoScopeM4ST3R42");
 		  //Person2 replace P2 with your name
 		  //and gburdell with your gt id
-		  Person2 p2 = new Person2("P2");
-		  toPrint += p2.toString("gburdell2");
+		  Person2 p2 = new Person2("RobertNoScopeM4ST3R42");
+		  toPrint += p2.toString("RobertNoScopeM4ST3R42");
 		  //Person3 replace P3 with your name
 		  //and gburdell3 with your gt id
-		  Person3 p3 = new Person3("P3");
-		  toPrint += p3.toString("gburdell3");
+		  Person3 p3 = new Person3("RobertNoScopeM4ST3R42");
+		  toPrint += p3.toString("RobertNoScopeM4ST3R42");
                   //Person4 replace P4 with your name
                   //and gburdell4 with your gt id
-                  Person4 p4 = new Person4("P4");
-                  toPrint += p4.toString("gburdell4");
+                  Person4 p4 = new Person4("RobertNoScopeM4ST3R42");
+                  toPrint += p4.toString("RobertNoScopeM4ST3R42");
                   //Person5 replace P4 with your name
                   //and gburdell5 with your gt id
-                  Person5 p5 = new Person5("P5");
-                  toPrint += p5.toString("gburdell5");
+                  Person5 p5 = new Person5("RobertNoScopeM4ST3R42");
+                  toPrint += p5.toString("RobertNoScopeM4ST3R42");
 		  
         System.out.println(toPrint);
         

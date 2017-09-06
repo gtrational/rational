877c7090860203ac47a166e1c18b65fe25323653
Daniel Hathcock <danielchathcock98@gmail.com>
2017-09-05 20:03:28 -0400

    Adds author to person3

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person3.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person3.java
@@ -2,36 +2,36 @@ package edu.gatech.oad.antlab.person;
 /**
  *  A simple class for person 3
  *  returns their name and a
- *  reversed string 
- *  
- *  @author  Bob
- *  @version 1.1
+ *  reversed string
+ *
+ *  @author  Bob, Daniel Hathcock
+ *  @version 1.2
  */
 public class Person3 {
-   /** Holds the persons real name */  
+    /** Holds the persons real name */
 	private String name;
-	
+
 	/**
 	 * The constructor, takes in the persons
 	 * name
 	 * @param pname the person's real name
 	 */
 	public Person3(String pname){
-	  name = pname;
+        name = pname;
 	}
-	
+
 	/**
 	 * Return a string rep of this object
 	 * that varies with an input string
 	 *
 	 * @param input the varying string
-	 * @return the string representing the 
+	 * @return the string representing the
 	 *         object
 	 */
 	public String toString(String input) {
-	  return name + calc(input);
+        return name + calc(input);
 	}
-	
+
 	/**
 	 * This method should take the string
 	 * input and return its reverse.
@@ -42,7 +42,11 @@ public class Person3 {
 	 * @return the reversed string
 	 */
 	private String calc(String input) {
-	  //Person 3 put your implementation here
-	  return null;
+        char[] newStr = new char[input.length()];
+        for (int i = 0; i < input.length(); i++) {
+            newStr[i] = input.charAt(input.length() - i - 1);
+        }
+
+        return new String(newStr);
 	}
 }

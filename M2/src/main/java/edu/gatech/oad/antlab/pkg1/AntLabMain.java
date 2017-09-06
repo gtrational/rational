4b839851e69aad11ca0959c324b6bf7e52b4ba49
James Lu <jameslu1998@gmail.com>
2017-09-05 20:42:15 -0400

    Adds name to AndLabMain

--- M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -73,8 +73,8 @@ import edu.gatech.oad.antlab.pkg2.*;
                   toPrint += p4.toString("903177344");
                   //Person5 replace P4 with your name
                   //and gburdell5 with your gt id
-                  Person5 p5 = new Person5("RobertNoScopeM4ST3R42");
-                  toPrint += p5.toString("gburdell5");
+                  Person5 p5 = new Person5("James Lu");
+                  toPrint += p5.toString("jlu342");
         System.out.println(toPrint);
     }
 

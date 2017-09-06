4f88e032f942631efaa6de91e8d112415a98f766
sherrysarkar <sherry.sarkar@outlook.com>
2017-09-05 20:07:44 -0400

    Completion of Person1 class

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person1.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person1.java
@@ -3,10 +3,10 @@ package edu.gatech.oad.antlab.person;
 /**
  *  A simple class for person 1
  *  returns their name and a
- *  modified string 
- *  
- *  @author Bob
- *  @version 1.1
+ *  modified string
+ *
+ *  @author Sherry Sarkar, and Bob
+ *  @version 1.2
  */
 public class Person1 {
   /** Holds the persons real name */
@@ -30,16 +30,17 @@ public class Person1 {
 	 * @return the modified string
 	 */
 	private String calc(String input) {
-	  //Person 1 put your implementation here
-	  return null;
+	  String rotate = input.substring(0, 2);
+      String ans = input.substring(2, input.length()) + rotate;
+	  return ans;
 	}
-	
+
 	/**
 	 * Return a string rep of this object
 	 * that varies with an input string
 	 *
 	 * @param input the varying string
-	 * @return the string representing the 
+	 * @return the string representing the
 	 *         object
 	 */
 	public String toString(String input) {

a297b813a2586a9f5713cfb26d13089e4d8b5967
Daniel Hathcock <danielchathcock98@gmail.com>
2017-09-05 20:42:05 -0400

    Add name to AntLabMain correctly

--- M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -65,7 +65,7 @@ import edu.gatech.oad.antlab.pkg2.*;
 		  toPrint += p2.toString("gburdell2");
 		  //Person3 replace P3 with your name
 		  //and gburdell3 with your gt id
-		  Person3 p3 = new Person3("P3");
+		  Person3 p3 = new Person3("Daniel Hathcock");
 		  toPrint += p3.toString("dhathcock3");
           //Person4 replace P4 with your name
           //and gburdell4 with your gt id

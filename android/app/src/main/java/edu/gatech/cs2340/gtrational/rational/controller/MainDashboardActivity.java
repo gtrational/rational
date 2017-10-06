c11290531628c63e906f757a62b6d95b7df63e80
James Lu <jameslu1998@gmail.com>
2017-10-06 16:10:19 -0400

    Adds fragment switching
    Adds screen switching through the nav drawer. 
    Moves logout functionality to settings.
    Changes FAB to plus.

--- android/app/src/main/java/edu/gatech/cs2340/gtrational/rational/controller/MainDashboardActivity.java
+++ android/app/src/main/java/edu/gatech/cs2340/gtrational/rational/controller/MainDashboardActivity.java
@@ -1,9 +1,11 @@
 package edu.gatech.cs2340.gtrational.rational.controller;
 
+import android.app.FragmentManager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -43,6 +45,11 @@ public class MainDashboardActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        setTitle("Dashboard");
+        DashboardFragment dashboardFragment = new DashboardFragment();
+        FragmentManager fragmentManager = getFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.fragment, dashboardFragment).commit();
     }
 
     @Override
@@ -70,13 +77,13 @@ public class MainDashboardActivity extends AppCompatActivity
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_logout) {
-            Intent intent = new Intent(this, MainActivity.class);
-            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
-            startActivity(intent);
-            finish();
-            return true;
-        }
+//        if (id == R.id.action_logout) {
+//            Intent intent = new Intent(this, MainActivity.class);
+//            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+//            startActivity(intent);
+//            finish();
+//            return true;
+//        }
 
         return super.onOptionsItemSelected(item);
     }
@@ -88,15 +95,28 @@ public class MainDashboardActivity extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.nav_camera) {
-            // Handle the camera action
+            setTitle("Dashboard");
+            DashboardFragment dashboardFragment = new DashboardFragment();
+            FragmentManager fragmentManager = getFragmentManager();
+            fragmentManager.beginTransaction().replace(R.id.fragment, dashboardFragment).commit();
         } else if (id == R.id.nav_gallery) {
-
+            setTitle("Nearby");
+            NearbyFragment nearbyFragment = new NearbyFragment();
+            FragmentManager fragmentManager = getFragmentManager();
+            fragmentManager.beginTransaction().replace(R.id.fragment, nearbyFragment).commit();
         } else if (id == R.id.nav_slideshow) {
-
+            setTitle("Map");
+            MapFragment mapFragment = new MapFragment();
+            FragmentManager fragmentManager = getFragmentManager();
+            fragmentManager.beginTransaction().replace(R.id.fragment, mapFragment).commit();
         } else if (id == R.id.nav_manage) {
-
+            setTitle("All Sightings");
+            ListFragment listFragment = new ListFragment();
+            FragmentManager fragmentManager = getFragmentManager();
+            fragmentManager.beginTransaction().replace(R.id.fragment, listFragment).commit();
         } else if (id == R.id.nav_share) {
-
+            Intent intent = new Intent(this, SettingsActivity.class);
+            startActivity(intent);
         } else if (id == R.id.nav_send) {
 
         }

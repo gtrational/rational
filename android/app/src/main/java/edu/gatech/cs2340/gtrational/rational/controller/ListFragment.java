3dd95848a806f4097149f8615dccbb43272ed7a6
James Lu <jameslu1998@gmail.com>
2017-10-11 02:17:50 -0400

    Implements listview and adapter

--- android/app/src/main/java/edu/gatech/cs2340/gtrational/rational/controller/ListFragment.java
+++ android/app/src/main/java/edu/gatech/cs2340/gtrational/rational/controller/ListFragment.java
@@ -6,6 +6,12 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 import edu.gatech.cs2340.gtrational.rational.R;
 
@@ -23,8 +29,29 @@ public class ListFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list, container, false);
+        View view =  inflater.inflate(R.layout.fragment_list, container, false);
+
+        String[][] testData = {
+                {"LLKSDKJLJLKSDF", "!@KE@!#!@$!@", "()*SUDJLFKLKJSDF"},
+                {"LLKSDKJLJLKSDF", "!@KE@!#!@$!@", "()*SUDJLFKLKJSDF"},
+                {"LLKSDKJLJLKSDF", "!@KE@!#!@$!@", "()*SUDJLFKLKJSDF"},
+                {"LLKSDKJLJLKSDF", "!@KE@!#!@$!@", "()*SUDJLFKLKJSDF"}
+        };
+
+        ArrayList<HashMap<String, String>> newList = new ArrayList<>();
+
+        for (int i = 0; i < testData.length; i++) {
+            HashMap<String, String> item = new HashMap<>();
+            item.put("line1", testData[i][0]);
+            item.put("line2", testData[i][1]);
+            item.put("line3", testData[i][2]);
+            newList.add(item);
+        }
+
+        ListView theList = (ListView) view.findViewById(R.id.listview);
+        SimpleAdapter sa = new SimpleAdapter(this.getActivity(), newList, R.layout.row_layout, new String[] {"line1", "line2", "line3"}, new int[] {R.id.line1, R.id.line2, R.id.line3});
+        theList.setAdapter(sa);
+        return view;
     }
 
 }

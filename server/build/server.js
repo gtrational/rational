30dfa4f7f6933041d3c3471b182fe1ce6dc64f62
Daniel Hathcock <danielchathcock98@gmail.com>
2017-10-10 22:23:53 -0400

    Removes node_modules and build

--- server/build/server.js
+++
@@ -1,7 +0,0 @@
-var express = require('express');
-var app = express();
-var http = require('http').Server(app);
-app.get('/', function (req, res) {
-    res.send("Hello World");
-});
-//# sourceMappingURL=server.js.map

5b2ceec8d6b42f98d369db0c73b28c0c0d17b5fc
Robert Pooley <rcpooley@gatech.edu>
2017-10-10 23:01:16 -0400

    Adds copy command and starts http server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,3 +1,8 @@
+console.log('----- Starting Rational Backend');
+
+//Load config
+var config = require('./config.json');
+
 var express = require('express');
 var app = express();
 
@@ -5,4 +10,8 @@ var http = require('http').Server(app);
 
 app.get('/', function(req, res) {
     res.send("Hello World");
+});
+
+http.listen(config.port, function () {
+    console.log('Listening on *:' + config.port);
 });

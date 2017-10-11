208bcb20e5bdc6695499d530663282d71aae6dd3
James Lu <jameslu1998@gmail.com>
2017-10-11 01:10:00 -0400

    Layouts new sighting screen

--- android/app/src/main/java/edu/gatech/cs2340/gtrational/rational/controller/NewSightingActivity.java
+++ android/app/src/main/java/edu/gatech/cs2340/gtrational/rational/controller/NewSightingActivity.java
@@ -2,7 +2,11 @@ package edu.gatech.cs2340.gtrational.rational.controller;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import edu.gatech.cs2340.gtrational.rational.R;
 
@@ -11,16 +15,39 @@ public class NewSightingActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setTitle("Create New Sighting");
+        setTitle("");
         setContentView(R.layout.activity_new_sighting);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_action_close);
+
+        Spinner spinner = (Spinner) findViewById(R.id.location_type);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.location_type, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        Spinner boroughSpinner = (Spinner) findViewById(R.id.borough_spinner);
+        ArrayAdapter<CharSequence> boroughAdapter = ArrayAdapter.createFromResource(this, R.array.boroughs, android.R.layout.simple_spinner_item);
+        boroughAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        boroughSpinner.setAdapter(boroughAdapter);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_new_sighting, menu);
+        return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case android.R.id.home:
+                finish();
+                return true;
+            case R.id.action_save:
+                // TODO: Add save functionality
                 finish();
                 return true;
         }
--- android/app/src/main/java/edu/gatech/cs2340/gtrational/rational/web/WebAPI.java
+++ android/app/src/main/java/edu/gatech/cs2340/gtrational/rational/web/WebAPI.java
@@ -12,6 +12,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.security.Timestamp;
 
 import edu.gatech.cs2340.gtrational.rational.model.User;
 

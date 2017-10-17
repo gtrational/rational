7fe71b2e7c7a59b678283d9e007ff45403da457a
Daniel Hathcock <danielchathcock98@gmail.com>
2017-10-17 04:46:04 -0400

    Adds listeners for updates to the model

--- android/app/src/main/java/edu/gatech/cs2340/gtrational/rational/model/Model.java
+++ android/app/src/main/java/edu/gatech/cs2340/gtrational/rational/model/Model.java
@@ -1,22 +1,91 @@
 package edu.gatech.cs2340.gtrational.rational.model;
 
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * Created by shyamal on 10/2/17.
  */
 
 public class Model {
-    private static Model instance = new Model();
-    private User user;
+    /**
+     * List of available topics
+     */
+    public static final String USER_UPDATE = "user.update";
+    public static final String RAT_SIGHTING_UPDATE = "rat_sighting.update";
 
+
+    private static Model instance = new Model();
+
+    private User user;
+    private List<RatSighting> ratSightings;
+
+    /**
+     * Map of listeners from topics to callbacks
+     */
+    private Map<String, List<ModelUpdateListener>> updateListeners;
 
     private Model() {
+        ratSightings = new ArrayList<>();
+        updateListeners = new HashMap<>();
     }
 
+    /**
+     * Static method to access singleton instance.
+     * @return singleton instance
+     */
     public static Model getInstance(){
         return instance;
     }
 
-    public void setUser(User user) {
-        this.user = user;
+    /**
+     * Register a new listener to the model on the given topic
+     *
+     * @param topic a string representing the topic of information being subscribed to
+     * @param listener a callback to be passed information about the update which occurred
+     */
+    public void registerListener(String topic, ModelUpdateListener listener) {
+        if (updateListeners.containsKey(topic)) {
+            updateListeners.get(topic).add(listener);
+        } else {
+            ArrayList<ModelUpdateListener> newListener = new ArrayList<>();
+            newListener.add(listener);
+
+            updateListeners.put(topic, newListener);
+        }
+    }
+
+    /**
+     * Public info to all listeners for a certain topic about an update to data in the model.
+     *
+     * @param topic topic to publish to
+     * @param updateInfo JSONObject containing info about what data in the model was updated.
+     */
+    public void publish(String topic, JSONObject updateInfo) {
+        if (!updateListeners.containsKey(topic)) {
+            return;
+        }
+
+        for (ModelUpdateListener listener : updateListeners.get(topic)) {
+            listener.callback(updateInfo);
+        }
+    }
+
+    public void updateUser(JSONObject userInfo) {
+        // TODO: Actually update user, after JSON format is agreed upon.
+        // user = new User(...);
+
+        publish(USER_UPDATE, userInfo);
+    }
+
+    public void updateRatSighting(JSONObject ratInfo) {
+        // TODO: Actually update a rat sighting
+        // ratSightings.get(...).setasdfasdf()
+
+        publish(RAT_SIGHTING_UPDATE, ratInfo);
     }
 }

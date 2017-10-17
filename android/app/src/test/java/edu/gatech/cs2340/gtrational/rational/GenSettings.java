a3bfbd39ac929daa8f79a731109d2ced13311720
Robert Pooley <rcpooley@gatech.edu>
2017-10-17 16:20:10 -0400

    Made a global/local config system for android

---
+++ android/app/src/test/java/edu/gatech/cs2340/gtrational/rational/GenSettings.java
@@ -0,0 +1,35 @@
+package edu.gatech.cs2340.gtrational.rational;
+
+import org.junit.Test;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by Robert on 10/17/2017.
+ */
+
+public class GenSettings {
+
+    private static final String defaultSettings = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n" +
+            "<resources>\n" +
+            "    <setting name=\"hosturl\">http://rational.tk:80</setting>\n" +
+            "    <setting name=\"arewelazy\">no</setting>\n" +
+            "</resources>";
+
+    @Test
+    public void generateSettingsXml() throws Exception {
+        File directory = new File("app/src/main/res/xml");
+        if (!directory.exists()) {
+            directory.mkdirs();
+        }
+        File out = new File(directory.getAbsolutePath() + "\\" + "settings.xml");
+        BufferedWriter writer = new BufferedWriter(new FileWriter(out));
+        writer.write(defaultSettings);
+        writer.close();
+    }
+
+}

9b5e8256ecddb6211ad78d6236299aa7b6d6c20c
Robert Pooley <rcpooley@gatech.edu>
2017-10-17 22:37:06 -0400

    Very functioanl

--- android/app/src/main/java/edu/gatech/cs2340/gtrational/rational/Callbacks.java
+++ android/app/src/main/java/edu/gatech/cs2340/gtrational/rational/Callbacks.java
@@ -9,14 +9,17 @@ import org.json.JSONObject;
 
 public class Callbacks {
 
+    @FunctionalInterface
     public interface VoidCallback {
         void callback();
     }
 
+    @FunctionalInterface
     public interface AnyCallback<T> {
         void callback(T object);
     }
 
+    @FunctionalInterface
     public interface JSONExceptionCallback<T> {
         void callback(T object) throws JSONException;
     }

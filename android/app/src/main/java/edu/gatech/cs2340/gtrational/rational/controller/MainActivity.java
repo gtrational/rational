6794ec2511ee2e54aba3ce221050c0f0193051fb
Robert Pooley <rcpooley@gatech.edu>
2017-10-18 21:52:25 -0400

    Updated lazy login

--- android/app/src/main/java/edu/gatech/cs2340/gtrational/rational/controller/MainActivity.java
+++ android/app/src/main/java/edu/gatech/cs2340/gtrational/rational/controller/MainActivity.java
@@ -1,12 +1,19 @@
 package edu.gatech.cs2340.gtrational.rational.controller;
 
 import android.content.Intent;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import edu.gatech.cs2340.gtrational.rational.R;
 import edu.gatech.cs2340.gtrational.rational.RationalApp;
+import edu.gatech.cs2340.gtrational.rational.model.Model;
+import edu.gatech.cs2340.gtrational.rational.model.web.WebAPI;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -45,10 +52,24 @@ public class MainActivity extends AppCompatActivity {
      * @param view
      */
     public void lazyLogin(View view) {
-        // WebAPI.fetchPrelimRatData(); was trying to test 
-        Intent intent = new Intent(this, MainDashboardActivity.class);
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-        startActivity(intent);
-        finish();
+        String username = "testuser";
+        String password = "testpass";
+        WebAPI.login(username, password, (WebAPI.LoginResult result) -> {
+            if (result.success) {
+                try {
+                    Model.getInstance().updateUser(new JSONObject().put("email", username).put("sessionID", result.sessionID).put("permLevel", result.permissionLevel.ordinal()));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                Intent intent = new Intent(this, MainDashboardActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+                finish();
+            } else {
+                Log.d("Tag", "YOU DUN MESSED UP AY AY RON");
+                Snackbar snackbar = Snackbar.make(view, "Login Failed", Snackbar.LENGTH_LONG);
+                snackbar.show();
+            }
+        });
     }
 }

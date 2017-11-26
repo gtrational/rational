5c22d634755c71039defff2f7e2d445ee2770db6
Daniel Hathcock <danielchathcock98@gmail.com>
2017-11-26 14:26:39 -0500

    Adds some mysql scrips

--- server/src/scripts/create_objects.ts
+++ server/src/scripts/create_objects.ts
@@ -4,15 +4,15 @@ import * as path from 'path';
 
 import config from '../config/config';
 
-const filePath = path.join(__dirname, '..', 'database', 'rat-create-db.sql')
+const filePath = path.join(__dirname, '..', 'database', 'rat-objects.sql')
 const queries = fs.readFileSync(filePath).toString();
 
 // console.log(queries);
 
 // Allow multiple statements in a mysql query
-config.mysqlRoot.multipleStatements = true;
+config.mysql.multipleStatements = true;
 
-let connection = mysql.createConnection(config.mysqlRoot);
+let connection = mysql.createConnection(config.mysql);
 connection.connect((err) => {
     if (err) {
         console.error('Could not connect with credentials ' + config.mysqlRoot + ': ' + err.stack);
@@ -25,7 +25,6 @@ connection.beginTransaction((err) => {
     if (err) {
         throw err;
     }
-    console.log('Creating database ratdb and user ratuser')
     connection.query(queries, (err, result) => {
         if (err) {
             connection.rollback(() => {
@@ -47,4 +46,3 @@ connection.beginTransaction((err) => {
 
 
 });
-
--- server/src/scripts/drop_objects.ts
+++ server/src/scripts/drop_objects.ts
@@ -4,15 +4,15 @@ import * as path from 'path';
 
 import config from '../config/config';
 
-const filePath = path.join(__dirname, '..', 'database', 'rat-create-db.sql')
+const filePath = path.join(__dirname, '..', 'database', 'rat-drop-objects.sql')
 const queries = fs.readFileSync(filePath).toString();
 
 // console.log(queries);
 
 // Allow multiple statements in a mysql query
-config.mysqlRoot.multipleStatements = true;
+config.mysql.multipleStatements = true;
 
-let connection = mysql.createConnection(config.mysqlRoot);
+let connection = mysql.createConnection(config.mysql);
 connection.connect((err) => {
     if (err) {
         console.error('Could not connect with credentials ' + config.mysqlRoot + ': ' + err.stack);
@@ -21,11 +21,12 @@ connection.connect((err) => {
     console.log('Connected as ' + JSON.stringify(config.mysqlRoot));
 });
 
+console.log('Warning: dropping all data in database ratdb')
+
 connection.beginTransaction((err) => {
     if (err) {
         throw err;
     }
-    console.log('Creating database ratdb and user ratuser')
     connection.query(queries, (err, result) => {
         if (err) {
             connection.rollback(() => {

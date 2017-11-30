250e4c196c12fdcec72e4a6574995d1a0b68db48
Daniel Hathcock <danielchathcock98@gmail.com>
2017-11-30 04:04:00 -0500

    Adds some basic framework stuff for web app

---
+++ web_app/src/components/Router.js
@@ -0,0 +1,2 @@
+import React from 'react'
+import { IndexRoute, Route, Router } from 'react-router'
--- web_app/src/index.js
+++ web_app/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import './index.css';
+import Router from './components/Router';
+
+ReactDOM.render(<Router/>, document.getElementById('root'));

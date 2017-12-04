83a977a7025e88c50611c3157d1a7ab6492b92c8
Robert Pooley <rcpooley@gatech.edu>
2017-12-04 18:57:24 -0500

    Adds WebAPI class with same functionality as android webapi

---
+++ windows/rational/Model.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rational
+{
+    public class Model
+    {
+
+        private static Model instance;
+
+        public static Model GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new Model();
+            }
+
+            return instance;
+        }
+
+        public LoginResult User;
+    }
+}
--- windows/rational/Program.cs
+++ windows/rational/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using rational;
 
 namespace WindowsFormsApp1
 {
@@ -14,6 +15,25 @@ namespace WindowsFormsApp1
         [STAThread]
         static void Main()
         {
+            WebAPI.LoginCallback cb = resp =>
+            {
+                Model.GetInstance().User = resp;
+                Console.WriteLine("Success: " + resp.Success);
+                Console.WriteLine("SessionID: " + resp.SessionID);
+
+                WebAPI.RatListCallback cb2 = resp2 =>
+                {
+                    for (var i = 0; i < resp2.Count; i++)
+                    {
+                        Console.WriteLine(resp2[i].UniqueKey);
+                    }
+                };
+
+                WebAPI.GetRatSightings(0, 20, cb2);
+            };
+
+            WebAPI.Login("testuser", "testpass", cb);
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainScreen());

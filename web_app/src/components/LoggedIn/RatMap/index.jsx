b68d687acdb2bb26279e8277ad5a7da2fd747c2b
Robert Pooley <rcpooley@gatech.edu>
2017-12-05 18:11:54 -0500

    Finished RatMap

--- web_app/src/components/LoggedIn/RatMap/index.jsx
+++ web_app/src/components/LoggedIn/RatMap/index.jsx
@@ -1,12 +1,9 @@
-import React, { Component } from 'react'
-
+import React, {Component} from 'react'
 
 export default class RatMap extends Component {
     render() {
         return (
-            <div>
-                <h1>RatMap</h1>
-            </div>
+            <iframe src="http://gooz.ml/map.html" style={{position: 'relative', left: 0, top: 0, width: '100%', height: '500px'}}></iframe>
         )
     }
 }
